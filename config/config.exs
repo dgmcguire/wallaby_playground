e127b820c53458e2f1e72b9ae57abd4777b9a22d
Dan McGuire <dan.mcguire@jpcycles.com>
2023-06-27 14:08:48 -0500

    makes chromedriver non-headless

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,7 @@ config :wallaby, base_url: "https://rz.stagezla.com/",
   max_wait_time: 10_000,
   js_errors: false,
   screenshot_dir: "#{File.cwd!}/ss",
-  screenshot_on_failure: true
+  screenshot_on_failure: true,
+  chromedriver: [
+    headless: false
+  ]
