a5d423e3ce0dd8964f41f0cbc94918bcd111c248
Peter <peter.licopantis@revzilla.com>
2023-07-31 12:40:29 -0400

    initial file

--- lib/wallaby_playground.ex
+++ lib/wallaby_playground.ex
@@ -1,9 +1,7 @@
 defmodule WallabyPlayground do
+  alias Wallaby.StaleReferenceError
   use Wallaby.DSL
   require ExUnit.Assertions
-
-
-
 
   def run do
 
@@ -20,13 +18,17 @@ defmodule WallabyPlayground do
         |> Query.count(:any)
       )
 
+    # fill in text box
     Element.fill_in(elem, with: "workinprogress")
 
+    # finds submit button and clicks it
     session
       |> find(Query.css("button[name=\"access_site\"][type=\"submit\"]"), fn(element) -> Element.click(element) end)
 
     session
       |> find(Query.css("a[href=\"/\"]"), fn(element) -> Element.click(element) end)
+
+    Process.sleep(2000)
 
     #hover over helmet selector
     session
@@ -44,13 +46,11 @@ defmodule WallabyPlayground do
     session
       |> find(Query.css("a.product-index-filters__facet-link.product-index-filters__facet-link--facet[data-discount-level=\"50\"][data-js=\"ProductCatalog.discountLink ProductCatalog.filterLink\"][href=\"#\"]"), fn(element) -> Element.click(element) end)
 
-    Process.sleep(2000)
-
     #click on first element
     session
       |> find(Query.css("a.product-index-results__product-tile.product-tile[data-index=\"0\"]"), fn(element) -> Element.click(element) end)
 
-    Process.sleep(5000)
+    Process.sleep(1000)
 
     # assert that the chat exists
     IO.puts(ExUnit.Assertions.assert(has_css?(session, "#chat-widget-minimized")))
@@ -81,8 +81,6 @@ defmodule WallabyPlayground do
     session
       |> find(Query.css("a.product-add-confirm__checkout-link.ui-link.ui-link--stacked[data-qa=\"view-cart\"][href=\"/cart\"]"), fn(element) -> Element.click(element) end)
 
-    Process.sleep(1000)
-
     # test if join banner exists
     IO.puts(ExUnit.Assertions.assert(has_css?(session, "div.cart-show__clp-banner.cart-show__clp-banner--non-member")))
 
@@ -93,13 +91,11 @@ defmodule WallabyPlayground do
         |> find(Query.css("dd.line-item-summary__value[data-js=\"LineItemTable.subtotal.value\"]"))
         |> Element.text()
 
-
     # obtaining subtotal and making it a float
     {subtotal, _} =
       subtotalString
         |> String.replace("$", "")
         |> Float.parse()
-
 
     # checking if shipping is correctly free
     if subtotal > 69.99 do
@@ -116,10 +112,13 @@ defmodule WallabyPlayground do
     session
       |> find(Query.xpath("/html/body/div[3]/main/div[2]/div[4]/div/a"), fn(element) -> Element.click(element) end)
 
+    Process.sleep(2000)
+
     # click checkout as guest
     session
     |> find(Query.xpath("/html/body/div[3]/main/div[2]/div[3]/div/a"), fn(element) -> Element.click(element) end)
 
+    # fill out shipping information
     fill_in(session, Query.text_field("Last Name"), with: "Licopantest")
 
     fill_in(session, Query.text_field("Address Line 1"), with: "151 Garrett Street")
@@ -143,27 +142,62 @@ defmodule WallabyPlayground do
     move_mouse_by(session, 10, 10)
     Browser.double_click(session)
 
-    Process.sleep(10000)
+    Process.sleep(1000)
 
-    IO.puts(page_source(session))
+    focus_frame(session, Query.xpath("/html/body/div[3]/main/form/div"))
 
-    session
-      |> find(Query.xpath("/html/body/div[3]/main/form/div/div/div[2]/div/div[1]/div/div[2]/div[2]/div"), fn(element) -> Element.click(element) end)
+    IO.puts(ExUnit.Assertions.assert(selected?(session, Query.xpath("/html/body/div[3]/main/form/div/div/div[3]/div/div[1]/div[2]/div/input[1]", visible: false))))
 
-    [continueButton | _]  = Browser.all(session, Query.button("Continue"))
-    move_mouse_by(session, 10, -10)
-    Element.click(continueButton)
-
-    #click(page, Query.radio_button("Radio Button 1"))
-
-    #session |> find(Query.xpath("/html/body/div[2]/div[1]/div/div[1]/a/img"), fn(element) -> Element.click(element) end)
-
+    [ _ | [testButton]] = find(session, Query.button("Continue", count: 2))
+    try do
+      Element.click(testButton)
+    rescue
+      e in StaleReferenceError -> IO.puts("An error occurred: " <> e.message)
+    after
+      IO.puts("HI")
+    end
 
     Process.sleep(1000)
 
+    # fill out (fake) credit card information
+    session
+      |> focus_frame(Query.xpath("/html/body/div[3]/main/form/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/label[1]/div[1]/div/iframe"))
+      |> fill_in(Query.xpath("//*[@id=\"credit-card-number\"]"), with: "4111111111111111")
+
+    focus_parent_frame(session)
+
+    session
+      |> focus_frame(Query.xpath("/html/body/div[3]/main/form/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/label[2]/div[1]/div/iframe"))
+      |> fill_in(Query.xpath("//*[@id=\"cvv\"]"), with: "400")
+
+    focus_parent_frame(session)
+
+    session
+      |> focus_frame(Query.xpath("/html/body/div[3]/main/form/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/label[1]/div[1]/div/iframe"))
+      |> click(Query.option("12 - December"))
+
+    focus_parent_frame(session)
+
+    session
+        |> focus_frame(Query.xpath("/html/body/div[3]/main/form/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[2]/label[2]/div[1]/div/iframe"))
+        |> click(Query.option("2029"))
+    focus_parent_frame(session)
+
+    # click continue and place order buttons to complete
+    [ _ | [contButton]] = find(session, Query.button("Continue", count: 2))
+    Element.click(contButton)
+
+    [ _ | [orderButton]] = find(session, Query.button("Place Order", count: 2))
+    try do
+      Element.click(orderButton)
+    rescue
+      e in StaleReferenceError -> IO.puts("An error occurred: " <> e.message)
+    after
+      IO.puts("It worked!")
+    end
+
     session
       |> Wallaby.Browser.take_screenshot()
-
 
   end
  end
